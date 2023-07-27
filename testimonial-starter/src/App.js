1cd27e5ce2b65aeab59308fed62e0dfddf22d536
Ayush Dwivedi <talkwithayush01@gmail.com>
2023-07-27 14:55:06 +0530

    Add files via upload

---
+++ testimonial-starter/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import reviews from "./data";
+import Testimonial from "./Testimonial";
+
+const App = () => {
+  return (
+    <div className="flex felx-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+     
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Our Testimonials</h1>
+        <div className="bg-violet-400 h-[4px] w-1/5 mt-2 mx-auto"></div>
+        <Testimonial reviews ={reviews}/>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ testimonial-starter/src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Himanshu Singh",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/D4D03AQGJwfAGSHPrHg/profile-displayphoto-shrink_800_800/0/1687018437047?e=1695859200&v=beta&t=uf0NgnhpmNii2ML_GpdI9BDRr4MjuHES4fsweZfpEzI",
+    text: "Enthusiastic team player, a fast learner with creative problem-solving skills, good coding and communication skills.",
+  },
+  {
+    id: 2,
+    name: "Vishal Yadav",
+    job: "Digital Marketing Intern",
+    image: "https://media.licdn.com/dms/image/D4D35AQFCploqGwDwFw/profile-framedphoto-shrink_400_400/0/1686117723901?e=1690974000&v=beta&t=dE8ZGOVa2276m9YQjni_YI0GTYzrWyrFp_HlAQspTtA",
+    text: "As a highly motivated and ambitious individual, I am thrilled to introduce myself as a Digital Marketing Intern.",
+  },
+  {
+    id: 3,
+    name: "Diksha Bajpai",
+    job: "Student",
+    image: "https://media.licdn.com/dms/image/D4D35AQEWUZEXLqLSqA/profile-framedphoto-shrink_400_400/0/1680540437521?e=1690974000&v=beta&t=i9BKeTs3U_OYR5XLbg33VZtbJQfdaZt6ER2N9dZDfEE",
+    text: "Pursuing MCA form KIET group of institutions || Self Optimistic person || Great learner || Self Motivated",
+  },
+  {
+    id: 4,
+    name: "Vikul Dhiman",
+    job: "MCA student",
+    image: "https://media.licdn.com/dms/image/D4D35AQGGJe9Zkc-jqg/profile-framedphoto-shrink_800_800/0/1681914302827?e=1690974000&v=beta&t=nvJFDDHEXgqHeD6r60vPH-A31ITCi1lTWkLiThdM7Zo",
+    text: "As a current student pursuing a Master's degree in Computer Applications at KIET Group of Institute, I am driven by a strong passion for achieving my goals.",
+  },
+  {
+    id: 5,
+    name: "Aditi Sharma",
+    job: "Engineering Student",
+    image: "https://media.licdn.com/dms/image/D4D03AQHO5V71deb7CQ/profile-displayphoto-shrink_800_800/0/1687943445453?e=1695859200&v=beta&t=0kjWvx-vNbMq_848tERzSq-Js2Sm8GWiBX971dJBGK8",
+    text: "Student at KIET Group of Institutions ||CSE(AIML) student || Learning C|| Aspiring to be a web developer ||",
+  },
+];
+
+export default reviews;
